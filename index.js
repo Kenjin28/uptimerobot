8cf57bb74a2c740d54179bbe8ee5398e646e42b3
Kenjin28 <79824075+Kenjin28@users.noreply.github.com>
2021-05-22 12:33:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://bot-awakens.glitch.me"]
+const urls = ["https://as-bot-backup.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
